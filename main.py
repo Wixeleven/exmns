1cce08ad16f6aa9a1750537b2ca01385fba9271f
Artjoms Kasperovičs <90187055+ar4cha@users.noreply.github.com>
2023-03-17 10:07:59 +0000

    code

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-import random, string
 from flask import Flask, render_template, request
+import wolframalpha
 
-app = Flask(  # Create a flask app
-	__name__,
-	template_folder='templates',  # Name of html file folder
-	static_folder='static'  # Name of directory for static files
-)
+app = Flask(__name__)
+client = wolframalpha.Client("P76EK3-XY6TKV2XYW")
 
-ok_chars = string.ascii_letters + string.digits
+@app.route("/")
+def index():
+    return render_template("index.html")
 
+@app.route("/search", methods=["POST"])
+def search():
+    topic = request.form["topic"]
+    query = topic
+    res = client.query(query)
+    result = ""
+    for pod in res.pods:
+        for sub in pod.subpods:
+            result += sub.plaintext + "\n\n"
+    return render_template("result.html", topic=topic, result=result)
 
-@app.route('/')  # What happens when the user visits the site
-def base_page():
-
-	random_num = random.randint(1, 100000)  # Sets the random number
-	return render_template(
-		'index.html',  # Template file path, starting from the templates folder. 
-		# Sets the variable random_number in the template
-	)
-
-
-@app.route('/2')
-def page_2():
-	rand_ammnt = random.randint(10, 100)
-	random_str = ''.join(random.choice(ok_chars) for a in range(rand_ammnt))
-	return render_template('site_2.html', random_str=random_str)
-
-
-
-if __name__ == "__main__":  # Makes sure this is the main process
-	app.run( # Starts the site
-		host='0.0.0.0',  # Establishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
-	)
+if __name__ == "__main__":
+    app.run(debug=True)
